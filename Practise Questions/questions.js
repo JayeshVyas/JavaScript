f88faf252b9361033dc339586712ab65660db470
Jayesh Vyas <jayeshvyas321@gmail.com>
2023-07-19 20:20:01 +0530

    Add files via upload

--- Practise Questions/questions.js
+++ Practise Questions/questions.js
@@ -64,3 +64,36 @@ function factorial(num){
   }
 }
 factorial(num);
+
+
+/**
+ * write a program in JS to find a number is armstrong or not in a given range?
+ * n1 = 8 n2  = 200
+ * armstrong b/w n1 and n2 = 8,9,153
+ * 153 = 1*1*1 + 5*5*5 + 3*3*3 = 153
+ * 1634 = 1*1*1*1 + 6*6*6*6 + 3*3*3*3 + 4*4*4*4 = 1634
+ */
+
+const n1 = parseInt(prompt('Enter lower range')); //8
+const n2 = parseInt(prompt('Enter higher range'));//200
+
+for(let i = n1 ; i<=n2 ; i++){
+    let digits = i.toString().length; //3
+
+    let sum = 0;
+
+    let tmp = i; //15
+
+    while(tmp > 0){
+
+        let rem = tmp % 10; //153%10 = 3;15%10 = 5 ; 1%10 = 1
+        sum += rem ** digits;//3^3+0 = 27;27+(5^3) = 125+27 = 152; 152+1^3 = 153
+
+        tmp = parseInt(tmp/10);// 153/10 = 15;15/10 = 1 ; 1/10 = 0
+
+    }
+
+    if(sum == i){
+        console.log(i);
+    }
+}
