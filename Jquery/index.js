3a640d076368216b2e5293b80c76fe106fa58dfb
Jayesh Vyas <jayeshvyas321@gmail.com>
2023-08-02 20:49:48 +0530

    Add files via upload

---
+++ Jquery/index.js
@@ -0,0 +1,8 @@
+/** lightweighted = it will not take much time in the compilation process.
+ * DOM manipulation
+ * Style or CSS manipulation
+ * Animation in JS
+ * Special Effects
+ * Special effects or events
+ * to use with AJAX calls
+*/
